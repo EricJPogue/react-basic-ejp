64f243634778e27acc9b34bf8a19aa2caea0eb3f
Eric Pogue <ericpogue@Erics-MBP.hsd1.il.comcast.net>
2024-05-13 20:40:25 -0500

    Update version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value} - V2.1</div>;
+  return <div>Hello {value} - V2.3</div>;
 }
 
 export default App;
